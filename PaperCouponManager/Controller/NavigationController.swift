d12e4f08570a75c886c9cee195fe35bc3f4d45b2
kobasa-tech <warped.trump@gmail.com>
2022-01-23 23:10:48 +0900

    ツールバーアクションの設定

--- PaperCouponManager/Controller/NavigationController.swift
+++ PaperCouponManager/Controller/NavigationController.swift
@@ -20,14 +20,44 @@ class NavigationController: UINavigationController,UINavigationControllerDelegat
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         
         // ツールバーボタンの設定
-        let register = UIBarButtonItem(title: "クーポン登録", style: .plain, target: self, action: nil)
-        let tagManage = UIBarButtonItem(title: "タグ管理", style: .plain, target: self, action: nil)
-        let systemEdit = UIBarButtonItem(title: "システム設定", style: .plain, target: self, action: nil)
+        let register = UIBarButtonItem(title: "クーポン登録", style: .plain, target: self, action: #selector(goRegisterVC))
+        let tagManage = UIBarButtonItem(title: "タグ管理", style: .plain, target: self, action: #selector(goTagManageVC))
+        let systemEdit = UIBarButtonItem(title: "システム設定", style: .plain, target: self, action: #selector(gosystemEditVC))
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
         
         // ツールバーボタンを配置
         viewController.toolbarItems = [register, flexibleSpace,  tagManage, flexibleSpace, systemEdit]
     }
+    
+    // ツールバーボタンをタップした時の処理
+    // クーポン登録画面を呼び出すメソッド
+    @objc func goRegisterVC() {
+        /*
+        let storyboard: UIStoryboard = self.storyboard!
+        let nextVC = storyboard.instantiateViewController(withIdentifier: "registerVC")
+        nextVC.modalPresentationStyle = .fullScreen // フルスクリーンで遷移
+        self.present(nextVC, animated: true, completion: nil)
+         */
+    }
+    // タグ管理画面を呼び出すメソッド
+    @objc func goTagManageVC() {
+        /*
+        let storyboard: UIStoryboard = self.storyboard!
+        let nextVC = storyboard.instantiateViewController(withIdentifier: "TagManageVC")
+        nextVC.modalPresentationStyle = .fullScreen
+        self.present(nextVC, animated: true, completion: nil)
+         */
+    }
+    // 設定画面を呼び出すメソッド
+    @objc func gosystemEditVC() {
+        /*
+        let storyboard: UIStoryboard = self.storyboard!
+        let nextVC = storyboard.instantiateViewController(withIdentifier: "systemEditVC")
+        nextVC.modalPresentationStyle = .fullScreen
+        self.present(nextVC, animated: true, completion: nil)
+         */
+    }
+
 
     /*
     // MARK: - Navigation
